5972aa836158094bf77e39b8c13fdd205c501c38
brdgsy <44277294+brdgsy@users.noreply.github.com>
2018-10-19 08:37:26 +0800

    54545454

--- hhh/src/com/wx/World.java
+++ hhh/src/com/wx/World.java
@@ -1,14 +1,21 @@
 package com.wx;
 //我随便写的一个测试 git 的类
 
-//修改后的版本
+//修改后的版本、、
+//我有改了 哈哈啊
 public class World {
 	private String s;
 	private String name;
+	pribate int id;
 	public void setS(String s){
 		this.s = s;
 	}
 	 public void setName(String name){
 		 this.name = name;
 	 }
+	public void setId(int id){
+		this.id = id;
+	}
+	
+	
 }
