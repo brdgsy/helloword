d8762b57bf25883fcd25c7828cbe753df9b03443
wx <328799597@qq.com>
2018-10-19 08:33:25 +0800

    Signed-off-by: wx <328799597@qq.com>

--- hhh/src/com/wx/World.java
+++ hhh/src/com/wx/World.java
@@ -1,8 +1,14 @@
 package com.wx;
 //我随便写的一个测试 git 的类
+
+//修改后的版本
 public class World {
 	private String s;
+	private String name;
 	public void setS(String s){
 		this.s = s;
 	}
+	 public void setName(String name){
+		 this.name = name;
+	 }
 }
